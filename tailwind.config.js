a5da314e376e95e3f59d495fa9a0b5ba6aa26156
mrdigitalvip-maker <mrdigitalvip@gmail.com>
2026-05-15 20:32:46 -0300

    fix: build funcionando

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+export default { content: ['./index.html', './src/**/*.{js,jsx}'], theme: { extend: {} }, plugins: [] }

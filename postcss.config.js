52b48ff07896d1a62379ba5ca6ef939fc88c4fe4
mrdigitalvip-maker <mrdigitalvip@gmail.com>
2026-05-16 00:32:19 -0300

    fix: use @tailwindcss/postcss in root postcss config for Vercel compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,9 @@
-export default { plugins: { tailwindcss: {}, autoprefixer: {} } }
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
+export default {
+	plugins: [
+		tailwindcss,
+		autoprefixer,
+	],
+}
